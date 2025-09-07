7707495bfd10e409bbf675deac2f2d7e277cd63d
Gopal-G0 <heyitsmegopal@gmail.com>
2025-09-07 22:23:07 +0530

    solved print  n to 1

---
+++ Recursion/print_NTo1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void Print(int n) {
+    
+    if(n == 1) {
+        cout << 1 << " ";
+        return;
+    }
+
+    cout << n << " ";
+    Print(n-1);
+}
+
+int main() {
+    
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    Print(n);
+    cout << endl;
+
+    return 0;
+}
---
+++ Recursion/tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+
+
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    Print(n);
+    cout << endl;
+    return 0;
+}
