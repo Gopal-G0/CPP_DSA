e1df3e83883b92324de751ea3f8d780a162d09ad
Gopal-G0 <heyitsmegopal@gmail.com>
2025-09-17 13:17:57 +0530

    learned about the depth of node

---
+++ Trees_DS/depthOfNode.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include<queue>
+using namespace std;
+
+// Tree Node Structure
+
+struct TreeNode
+{
+
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+
+    // Constructor
+    TreeNode(int val)
+    {
+
+        left = right = NULL;
+        data = val;
+    }
+};
+
+void insert(TreeNode *root, int val)
+{
+    queue<TreeNode *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        TreeNode* temp = q.front();
+        q.pop();
+
+        // If left child is empty → insert here
+        if (temp->left == NULL)
+        {
+            temp->left = new TreeNode(val);
+            return;
+        }
+        else
+        {
+            q.push(temp->left);
+        }
+
+        // If right child is empty → insert here
+        if (temp->right == NULL)
+        {
+            temp->right = new TreeNode(val);
+            return;
+        }
+        else
+        {
+            q.push(temp->right);
+        }
+    }
+}
+
+// Inorder Traversal to check structure
+void inorder(TreeNode *root)
+{
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int findDepth(TreeNode* root, int target, int depth = 0) {
+
+    if(root == nullptr) return -1;   // Base Case: Tree is empty or target not found
+
+    if(root -> data == target) return depth;  // if current Node matches target then -> return the depth
+
+    // look for left subtree
+    int leftDepth = findDepth(root -> left, target, depth + 1);
+    if(leftDepth != -1) return leftDepth;   // if found in left subtree
+
+    return findDepth(root -> right, target, depth + 1);
+}
+
+int main() {
+
+    TreeNode* root = new TreeNode(1);
+    insert(root,2);
+    insert(root,4);
+    insert(root,6);
+    insert(root,1);
+    insert(root,8);
+
+    inorder(root);
+    cout << endl;
+
+    cout << findDepth(root,4) << endl;
+    return 0;
+}
